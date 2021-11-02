516e262a5643a75538ba494ce431bbdf383bfe00
xiaowu-java02 <1241080852@qq.com>
2021-11-02 16:58:29 +0800

    Github登录获取用户数据

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,33 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callabck(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("a86ff4e080a997082ea4");
+        accessTokenDTO.setClient_secret("ea85b433af027e4ba11ab848b193dc3aa327ddeb");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = GithubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+
+}
