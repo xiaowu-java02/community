0ed243a05716e3ad4bdf2197f5f08e1bd07b7360
xiaowu-java02 <1241080852@qq.com>
2021-11-10 17:06:42 +0800

    Interceptor

--- src/main/java/life/majiang/community/controller/ProfileController.java
+++ src/main/java/life/majiang/community/controller/ProfileController.java
@@ -1,11 +1,8 @@
 package life.majiang.community.controller;
 
-import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 import life.majiang.community.dto.PaginationDTO;
-import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.User;
 import life.majiang.community.service.QusertionService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QusertionService qusertionService;
@@ -31,21 +25,7 @@ public class ProfileController {
                           @RequestParam(name = "page", defaultValue = "1") Integer page,
                           @RequestParam(name = "size", defaultValue = "5") Integer size) {
 
-        User user = null;
-        Cookie cookies[] = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
+        User user = (User)request.getSession().getAttribute("user");
         if (user == null) {
             return "redirect:/";
         }
