90ab6d52f39a3b8894a297c676be13cf11833a93
xiaowu-java02 <1241080852@qq.com>
2021-11-13 01:23:24 +0800

    Implement question reply-login without refreshing the page

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -11,7 +11,19 @@ function post(){
             "type":1
         }),
         success: function (response) {
-            console.log(response);
+            if (response.code == 200){
+                $("#comment_section").hide();
+            }else {
+                if (response.code == 2003){
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=c0cb44251cf59371b35c&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+                }else {
+                    alert(response.message);
+                }
+            }
         },
         dataType: "json"
     });
