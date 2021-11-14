3d604b59bba19908f98330460f5d868855596f6f
xiaowu-java02 <1241080852@qq.com>
2021-11-14 15:41:50 +0800

    Optimize the reply function and question details page

--- src/main/java/life/majiang/community/controller/CommentController.java
+++ src/main/java/life/majiang/community/controller/CommentController.java
@@ -6,6 +6,7 @@ import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.model.Comment;
 import life.majiang.community.model.User;
 import life.majiang.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,10 @@ public class CommentController {
         Object user = (User)request.getSession().getAttribute("user");
         if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
+        }
+
+        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
+            return ResultDTO.errorOf(CustomizeErrorCode.COMMENT_IS_EMPTY);
         }
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
--- src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
@@ -7,7 +7,8 @@ public enum CustomizeErrorCode implements ICustomizeErrorCode{
     NO_LOGIN(2003,"当前操作需要登录，请登录后重试"),
     SYS_ERROR(2004,"服务异常，稍后再试！！！"),
     TYPE_PARAM_WRONG(2005,"评论类型错误或不存在"),
-    COMMENT_NOT_FOUND(2006,"你回复的评论不在了，要不要换个试试")
+    COMMENT_NOT_FOUND(2006,"你回复的评论不在了，要不要换个试试"),
+    COMMENT_IS_EMPTY(2007,"输入内容不能为空")
     ;
 
     @Override
